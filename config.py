fd97fbd8107b0e118b9a1f8c5e3cedc92776a445
dima <menshikov.dmitry@gmail.com>
2024-05-12 13:42:55 +1000

    port hotfix

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ API_TOKEN = os.getenv('API_TOKEN')
 assert API_TOKEN is not None, 'API_TOKEN enviromental variable must be provided'
 
 HOSTNAME = os.getenv('HOSTNAME', 'localhost')
-PORT = 8000
+PORT = os.getenv('PORT', 8000)
 
 WEBSITE_API_URL = f"http://{HOSTNAME}:{PORT}/"
